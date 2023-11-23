cf0429fdfd24bf7c825cd0b41b48040ec8f78fdf
jim <jim.otieno@student.moringaschool.com>
2023-11-23 18:21:49 +0300

    completed

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,67 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
+    names=[food['name'] for food in spicy_foods]
+    return names
     pass
+
+print(get_names(spicy_foods))
 
 def get_spiciest_foods(spicy_foods):
+    spiciest=[food for food in spicy_foods if food['heat_level']>5]
+    return spiciest
     pass
+
+print(get_spiciest_foods(spicy_foods))
 
 def print_spicy_foods(spicy_foods):
+    for food in spicy_foods:
+        heat_level = "🌶" * food['heat_level']
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
     pass
+
+print_spicy_foods(spicy_foods)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    cuisine=[food for food in spicy_foods if cuisine==food['cuisine']]
+    return cuisine[0]
     pass
+
+print (get_spicy_food_by_cuisine(spicy_foods, "American"))
+print (get_spicy_food_by_cuisine(spicy_foods, "Thai"))
+
 
 def print_spiciest_foods(spicy_foods):
+
+    spiciest_list = get_spiciest_foods(spicy_foods)
+    
+    for food in spiciest_list:
+        heat_level = "🌶" * food['heat_level']
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
     pass
+
+
+print_spiciest_foods(spicy_foods)
 
 def get_average_heat_level(spicy_foods):
+    average=sum(food['heat_level'] for food in spicy_foods)
+    return average/3
     pass
 
+print(get_average_heat_level(spicy_foods))
+
 def create_spicy_food(spicy_foods, spicy_food):
+    spicera= {    
+            'name': 'Griot',
+            'cuisine': 'Haitian',
+           'heat_level': 10,
+         }
+    existing_food = [food for food in spicy_foods if spicy_food == food['name']]
+    
+    if not existing_food:
+        # If the provided spicy_food doesn't exist, add spicera to the list
+        spicy_foods.append(spicera)
+    
+    return spicy_foods
     pass
+print(create_spicy_food(spicy_foods,"Griot"))
